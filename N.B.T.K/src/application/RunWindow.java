51ee440e0988114672a88ef4842a0f36a02873bb
Unknown <bchristyanton@trentu.ca>
2018-03-21 04:37:23 -0400

    JAVA APP MAIN WINDOW

---
+++ N.B.T.K/src/application/RunWindow.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.event.ActionEvent;
+//import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+
+public class RunWindow {
+
+	@FXML
+//	String message;
+//	private Label myMessage;
+	public void sales(ActionEvent event){
+
+	 System.out.println("hey");
+}
+
+
+
+
+
+}
+
