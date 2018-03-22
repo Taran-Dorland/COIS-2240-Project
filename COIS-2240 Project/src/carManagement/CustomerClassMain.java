da58a2717be5302f07feec854c3469d696336185
Taran-Dorland <tarandorland@protonmail.com>
2018-03-22 15:38:34 -0400

    Merged runwindow, main javafx into carmanagement package

--- COIS-2240 Project/src/carManagement/CustomerClassMain.java
+++ COIS-2240 Project/src/carManagement/CustomerClassMain.java
@@ -1,9 +1,6 @@
 
 public class CustomerClassMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
---
+++ COIS-2240 Project/src/carManagement/RunWindow.java
@@ -0,0 +1,23 @@
+package carManagement;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+
+public class RunWindow {
+
+	@FXML
+	//String message;
+	//private Label myMessage;
+	public void sales(ActionEvent event){
+
+		System.out.println("hey");
+	}
+
+
+
+
+
+}
+
