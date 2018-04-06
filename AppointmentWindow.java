0e6e844157c4bc91c6819209ea69e0baced56025
kkill323 <35272042+kkill323@users.noreply.github.com>
2018-04-06 14:16:28 -0400

    Add files via upload

--- AppointmentWindow.java
+++ AppointmentWindow.java
@@ -1,6 +1,20 @@
-package sample;
+package application;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
 import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,158 +31,297 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
 public class AppointmentWindow {
-    public static void addAppointment(String title, String message) { //should probably not make it static for security
-
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
-        window.setTitle(title);
-        window.setMinHeight(400);
-        window.setMinWidth(400);
-
-        //GridPane with 10px padding around edge
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(10);
-
-        //Name Label - constrains use (child, column, row)
-        Label nameLabel = new Label("User ID:");
-        GridPane.setConstraints(nameLabel, 0, 0);
-
-        Label dateLabel = new Label ("date: ");
-        GridPane.setConstraints(dateLabel, 0, 1);
-
-        Label typeLabel = new Label ("booking type: ");
-        GridPane.setConstraints(typeLabel, 0, 2);
-
-        Label phoneLabel = new Label ("Phone number: ");
-        GridPane.setConstraints(phoneLabel, 0, 3);
-
-        Label emailLabel = new Label ("email: ");
-        GridPane.setConstraints(emailLabel, 0, 4);
-
-        //Name Input
-        TextField dateInput = new TextField();
-        dateInput.setPromptText("dd/mm/yyyy");
-        GridPane.setConstraints(dateInput, 1, 1);
-
-        //user Input
-        TextField userInput = new TextField();
-        userInput.setPromptText("Numbers only");
-        GridPane.setConstraints(userInput, 1, 0);
-
-        //type Input
-        TextField typeInput = new TextField();
-        typeInput.setPromptText("booking type");
-        GridPane.setConstraints(typeInput, 1, 2);
-
-//        //phone Input
-        TextField phoneInput = new TextField();
-        phoneInput.setPromptText("555-555-5555");
-        GridPane.setConstraints(phoneInput, 1, 3);
-
-        //email Input
-        TextField emailInput = new TextField();
-        emailInput.setPromptText("johnsmith@sample.com");
-        GridPane.setConstraints(emailInput, 1, 4);
-
-        //Login
-        Button loginButton = new Button("ADD information");
-        GridPane.setConstraints(loginButton, 1, 8);
-        loginButton.setOnAction(e -> {
-
-            isInt(userInput, userInput.getText());
-            //validateEmail(emailInout, emailInput.getText());
-            //IsPhone(phoneInput, phoneInput.getText());
-            //isDate(dateInput, dateInput.getText());;
 
 
-        });
+	private static Connection dbConn = null;
 
-        Button goBackToAppointment = new Button("GO BACK to SALES");
-        GridPane.setConstraints(goBackToAppointment, 1, 9);
-        goBackToAppointment.setOnAction(e -> window.close());
 
-        //Add everything to grid
-        grid.getChildren().addAll(nameLabel,dateLabel, typeLabel, phoneLabel, emailLabel, userInput, dateInput,phoneInput, emailInput,typeInput,  loginButton , goBackToAppointment);
+	public static void addAppointment(String title, String message) { //should probably not make it static for security
 
-        Scene scene = new Scene(grid, 400, 400);
-        window.setScene(scene);
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
+		window.setTitle(title);
+		window.setMinHeight(400);
+		window.setMinWidth(400);
 
-        window.showAndWait();
-    }
-    public static boolean isInt(TextField input, String message ) {
+		//GridPane with 10px padding around edge
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(8);
+		grid.setHgap(10);
 
-        try {
-            int ID = Integer.parseInt(message);
-            return true;
-        } catch (NumberFormatException e) {
+		//---------------------------------------------------------
 
-            return false;
-        }
 
-    }
-   // private boolean validateEmail(TextField input2, String message){
+		//Add auto-generating event id
+		//Add description
 
-   // }
+		//---------------------------------------------------------
 
 
 
-    public static void removeAppointment(String title, String message) { //should probably not make it static for security
+		//Name Label - constrains use (child, column, row)
+		Label nameLabel = new Label("User ID:");
+		GridPane.setConstraints(nameLabel, 0, 0);
 
-        Stage window2 = new Stage();
-        window2.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
-        window2.setTitle(title);
-        window2.setMinHeight(400);
-        window2.setMinWidth(400);
+		Label dateLabel = new Label ("date: ");
+		GridPane.setConstraints(dateLabel, 0, 1);
 
-        Label servicesLabel = new Label();
-        servicesLabel.setText(message);
+		Label typeLabel = new Label ("booking type: ");
+		GridPane.setConstraints(typeLabel, 0, 2);
 
-        VBox serviceLay = new VBox(20);
-        serviceLay.getChildren().addAll(servicesLabel);
+		Label phoneLabel = new Label ("Phone number: ");
+		GridPane.setConstraints(phoneLabel, 0, 3);
 
-        Scene serviceScene = new Scene(serviceLay);
-        window2.setScene(serviceScene);
-        window2.showAndWait();
-    }
+		Label emailLabel = new Label ("email: ");
+		GridPane.setConstraints(emailLabel, 0, 4);
 
-    public static void editAppointment(String title, String message) { //sorry i have no clue how to do this
-        Stage window3 = new Stage();
-        window3.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
-        window3.setTitle(title);
-        window3.setMinHeight(400);
+        Label emailLabel2 = new Label ("email2: ");
+        GridPane.setConstraints(emailLabel2, 0, 4);
+		//Name Input
+		TextField dateInput = new TextField();
+		GridPane.setConstraints(dateInput, 1, 1);
 
-        Label servicesLabel = new Label();
-        servicesLabel.setText(message);
+		//user Input
+		TextField userInput = new TextField();
+		GridPane.setConstraints(userInput, 1, 0);
 
-        VBox serviceLay = new VBox(20);
-        serviceLay.getChildren().addAll(servicesLabel);
+		//type Input
+		TextField typeInput = new TextField();
+		GridPane.setConstraints(typeInput, 1, 2);
 
-        Scene serviceScene = new Scene(serviceLay);
-        window3.setScene(serviceScene);
-        window3.showAndWait();
+		//phone Input
+		TextField phoneInput = new TextField();
+		GridPane.setConstraints(phoneInput, 1, 3);
 
-    }
+		//email Input
+		TextField emailInput = new TextField();
+		GridPane.setConstraints(emailInput, 1, 4);
 
-    public static void searchAppointment(String title, String message) {
+		//Login
+		
+		Button loginButton = new Button("ADD information");
+		GridPane.setConstraints(loginButton, 1, 8);
+		
+		dateInput.setPromptText(" dd/mm/yyyy ");
+		userInput.setPromptText(" Numbers only ");
+		typeInput.setPromptText(" booking type ");
+		phoneInput.setPromptText(" 555-555-5555 ");
+		emailInput.setPromptText(" johnsmith@sample.com ");
+
+		loginButton.setOnAction(e -> {
+
+			//Aquire values from text fields
+			int custID = Integer.parseInt(userInput.getText());
+			String bookingType = typeInput.getText();
+			String bookingDesc = "";
+			String bookingDate = dateInput.getText();
+			String bookingEmail = emailInput.getText();
+			String bookingPhone = phoneInput.getText();
+
+			try {
+				//Use values and insert into database
+				dbConn = DriverManager.getConnection("jdbc:sqlite:testdb.db");
+
+				String sql = "INSERT INTO booking(id, type, description, date, email, phoneNumber) VALUES(?,?,?,?,?,?)";
+
+				PreparedStatement ps = dbConn.prepareStatement(sql);
+
+				ps = dbConn.prepareStatement(sql);
+				ps.setInt(1, custID);
+				ps.setString(2, bookingType);
+				ps.setString(3, bookingDesc);
+				ps.setString(4, bookingDate);
+				ps.setString(5, bookingEmail);
+				ps.setString(6, bookingPhone);
+
+				ps.executeUpdate();
+
+				dbConn.close();
+
+			} catch (Exception e2) {
+				System.err.println(e2.getClass().getName() + ": " + e2.getMessage());
+				System.exit(0);
+			}
+
+			//public void onAddItem(ActionEvent event) {
+				//Form Validation
+				
+			//	boolean email = FormValidation.textFieldNotEmpty(emailInput, emailLabel, "Email is required");
+				
+			//}
+			//isInt(userInput, userInput.getText());
+			//validateEmail(emailInout, emailInput.getText());
+			  
+			//IsPhone(phoneInput, phoneInput.getText());
+			//isDate(dateInput, dateInput.getText());;
+		
+		});
+		
+		
+		Button goBackToAppointment = new Button("GO BACK to SALES");
+		GridPane.setConstraints(goBackToAppointment, 1, 9);
+		goBackToAppointment.setOnAction(e -> window.close());
+
+		//Add everything to grid
+		grid.getChildren().addAll(nameLabel,dateLabel, typeLabel, phoneLabel, emailLabel, emailLabel2, userInput, dateInput,phoneInput, emailInput,typeInput,  loginButton , goBackToAppointment);
+
+		Scene scene = new Scene(grid, 400, 400);
+		window.setScene(scene);
+
+		window.showAndWait();
+	}
+	  public boolean isPhoneNumberValid(String bookingPhone) {
+	        char[] UserId = bookingPhone.toCharArray();
+	        // check if user id is of length 10
+	        if (UserId.length != 10) {
+	            return false;
+	        } else {
+	            for (char i : UserId) {
+	                if (!(i >= 48 && i <= 57)) // Check if all the character in user name are nnumericals.
+	                {
+	                    return false;
+	                }
+	            }
+	        }
+	        return true;
+	    }
+	  
+	public static boolean isInt(TextField input, String message ) {
+
+		try {
+			int ID = Integer.parseInt(message);
+			return true;
+		} catch (NumberFormatException e) {
+
+			return false;
+		}
+
+	}
+	
+	 public boolean isUserIdValid(String custID) {
+	        char[] UserId = custID.toCharArray();
+	        // check if user id is of length 10
+	        if (UserId.length != 10) {
+	            return false;
+	        } else {
+	            for (char i : UserId) {
+	                if (!(i >= 48 && i <= 57)) // Check if all the character in user name are nnumericals.
+	                {
+	                    return false;
+	                }
+	            }
+	        }
+	        return true;
+	    }
+	 
+	 public boolean isDateValid(String bookingDate ) {
+
+	        // check if the length of dat is 10 (dd/mm/yyyy)
+	        if (bookingDate.length() != 10) {
+	            return false;
+	        } else {
+	            String[] div = bookingDate.split("/");
+	            if ((div.length) == 3) {
+	                try {
+	                    int date = Integer.parseInt(div[0]);
+	                    int month = Integer.parseInt(div[1]);
+	                    // int year = Integer.parseInt(div[2]);
+	                    if (!(date >= 1 && date <= 31) || !(month >= 1 && month <= 12)) {
+	                        return false;
+	                    }
+	                } catch (NumberFormatException e) {
+	                    return false;
+	                }
+	            } else {
+	                return false;
+	            }
+	        }
+
+	        return true;
+	    }
+	 
+	 public boolean isUserInputValid(String userInput) {
+	        char[] input = userInput.toCharArray();
+
+	        for (char i : input) {
+	            if (!(i >= 48 && i <= 57)) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
 
 
-        Stage window4 = new Stage();
-        window4.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
-        window4.setTitle(title);
-        window4.setMinHeight(400);
+	// private boolean validateEmail(TextField input2, String message){
 
-        Label servicesLabel = new Label();
-        servicesLabel.setText(message);
+	// }
 
-        VBox serviceLay = new VBox(20);
-        serviceLay.getChildren().
- 
-                addAll(servicesLabel);
 
-        Scene serviceScene = new Scene(serviceLay);
-        window4.setScene(serviceScene);
-        window4.showAndWait();
-    }
+
+	public static void removeAppointment(String title, String message) { //should probably not make it static for security
+
+		Stage window2 = new Stage();
+		window2.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
+		window2.setTitle(title);
+		window2.setMinHeight(400);
+		window2.setMinWidth(400);
+
+		Label servicesLabel = new Label();
+		servicesLabel.setText(message);
+
+		VBox serviceLay = new VBox(20);
+		serviceLay.getChildren().addAll(servicesLabel);
+
+		Scene serviceScene = new Scene(serviceLay);
+		window2.setScene(serviceScene);
+		window2.showAndWait();
+	}
+
+	public static void editAppointment(String title, String message) { //sorry i have no clue how to do this
+		Stage window3 = new Stage();
+		window3.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
+		window3.setTitle(title);
+		window3.setMinHeight(400);
+
+		Label servicesLabel = new Label();
+		servicesLabel.setText(message);
+
+		VBox serviceLay = new VBox(20);
+		serviceLay.getChildren().addAll(servicesLabel);
+
+		Scene serviceScene = new Scene(serviceLay);
+		window3.setScene(serviceScene);
+		window3.showAndWait();
+
+	}
+
+	public static void searchAppointment(String title, String message) {
+
+
+		Stage window4 = new Stage();
+		window4.initModality(Modality.APPLICATION_MODAL); //this forces the user to complete the window,so they cant go back
+		window4.setTitle(title);
+		window4.setMinHeight(400);
+
+		Label servicesLabel = new Label();
+		servicesLabel.setText(message);
+
+		VBox serviceLay = new VBox(20);
+		serviceLay.getChildren().
+
+		addAll(servicesLabel);
+
+		Scene serviceScene = new Scene(serviceLay);
+		window4.setScene(serviceScene);
+		window4.showAndWait();
+	}
+	
+	
+	
+	
+	//overide the for the prompt
+	public void initialize (URL arg0, ResourceBundle arg1) {
+		
+	}
 }
